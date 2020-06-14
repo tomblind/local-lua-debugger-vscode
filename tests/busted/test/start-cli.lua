2958f4c43e6c95ba26f15a9e258faa5b5645c4c2
Tom <tomblind@users.noreply.github.com>
2020-06-14 06:14:38 -0600

    added errors to busted tests

--- tests/busted/test/start-cli.lua
+++ tests/busted/test/start-cli.lua
@@ -1,7 +1,14 @@
 if os.getenv("LOCAL_LUA_DEBUGGER_VSCODE") == "1" then
-    require("lldebugger").start()
+    local lldebugger = require("lldebugger")
+    local d = describe
+    describe = function(name, fn) d(name, function() lldebugger.call(fn) end) end --allow breaking on errors in tests
+    lldebugger.start()
 end
 
 describe("a test", function()
     print("FUBAR")
+end)
+
+describe("an error", function()
+    x = y / z
 end)
--- tests/busted/test/start-interpreter.lua
+++ tests/busted/test/start-interpreter.lua
@@ -1,9 +1,16 @@
-if os.getenv("LOCAL_LUA_DEBUGGER_VSCODE") == "1" then
-    require("lldebugger").start()
-end
-
 require("busted.runner")()
+
+if os.getenv("LOCAL_LUA_DEBUGGER_VSCODE") == "1" then
+    local lldebugger = require("lldebugger")
+    local d = describe
+    describe = function(name, fn) d(name, function() lldebugger.call(fn) end) end --allow breaking on errors in tests
+    lldebugger.start()
+end
 
 describe("a test", function()
     print("FUBAR")
+end)
+
+describe("an error", function()
+    x = y / z
 end)
