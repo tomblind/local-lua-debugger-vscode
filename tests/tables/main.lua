445b298f09aa8407b7c1c467f0d6f3bd635ebd12
Tom <tomblind@users.noreply.github.com>
2022-04-03 17:36:31 -0600

    additional table tests

--- tests/tables/main.lua
+++ tests/tables/main.lua
@@ -20,11 +20,18 @@ local mt =
     __len = function() return 42 end,
     __add = function(self, other) return make() end,
     __tostring = function() return "foobar" end,
+    __metatable = "hidden"
 }
 
 make = function()
     return setmetatable({}, mt)
 end
+
+local empty = {}
+local emptyWithMt = setmetatable({}, {__index = {foo = "FOO"}})
+local emptyWithLenMt = setmetatable({}, {__len = function() return 42 end})
+local emptyWithBadLenMt = setmetatable({}, {__len = function() error("FUBAR") end})
+local emptyWithBadIndexMt = setmetatable({}, {__index = function(s, k) error("FUBAR") end})
 
 local a = make()
 local b = make()
